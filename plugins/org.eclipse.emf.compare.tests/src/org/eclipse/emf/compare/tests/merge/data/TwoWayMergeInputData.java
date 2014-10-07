35a05ddc946a15762cd38e6f5f67586ab2f73f19
Alexandra Buzila <alexandra.buzila@gmail.com>
2014-10-07 10:40:11 +0200

    Isolated failing test case from seed 436.

--- plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/data/TwoWayMergeInputData.java
+++ plugins/org.eclipse.emf.compare.tests/src/org/eclipse/emf/compare/tests/merge/data/TwoWayMergeInputData.java
@@ -64,4 +64,12 @@ public class TwoWayMergeInputData extends AbstractInputData {
 	public Resource getMoveToNewContainerInADifferentOrderR2LRight() throws IOException {
 		return loadFromClassLoader("twoway/movetonewcontainerinadifferentorder/rtl/right.nodes");
 	}
+
+	public Resource getFuzzy436R2LLeft() throws IOException {
+		return loadFromClassLoader("twoway/fuzzy436/left.nodes");
+	}
+
+	public Resource getFuzzy436R2LRight() throws IOException {
+		return loadFromClassLoader("twoway/fuzzy436/right.nodes");
+	}
 }
