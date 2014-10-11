056d1f980dc65246ddf9aaff02daf494807c8058
Philip Langer <planger@eclipsesource.com>
2014-10-11 19:56:36 +0200

    Simplifies three-way fuzz test (mutate one side only for now)

--- plugins/org.eclipse.emf.compare.tests.fuzzy/src/org/eclipse/emf/compare/tests/ThreeWayFuzzyTest.java
+++ plugins/org.eclipse.emf.compare.tests.fuzzy/src/org/eclipse/emf/compare/tests/ThreeWayFuzzyTest.java
@@ -12,7 +12,6 @@ package org.eclipse.emf.compare.tests;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
@@ -23,9 +22,7 @@ import org.eclipse.emf.common.notify.Notifier;
 import org.eclipse.emf.common.util.BasicMonitor;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.compare.Comparison;
-import org.eclipse.emf.compare.ConflictKind;
 import org.eclipse.emf.compare.Diff;
-import org.eclipse.emf.compare.DifferenceSource;
 import org.eclipse.emf.compare.EMFCompare;
 import org.eclipse.emf.compare.merge.BatchMerger;
 import org.eclipse.emf.compare.merge.IBatchMerger;
@@ -47,12 +44,10 @@ import com.google.common.collect.Sets;
  * Fuzz test for three-way differencing and merging.
  * <p>
  * This test generates an origin version and creates two copies, a left-hand side copy and a right-hand side
- * copy. These two copies are then mutated before a three-way comparison is performed. Next, all
- * non-conflicting changes are applied to the left-hand side model (right-to-left) and all changes (including
- * conflicting ones) are applied to the right-hand side model (left-to-right). Ultimately, this test checks
- * whether both resulting models are equal to each other, which should be true since both versions should be
- * the origin version plus all changes from the left-hand side and all non-conflicting changes from the
- * right-hand side.
+ * copy. One of these two copies is then mutated before a three-way comparison is performed. Next, all changes
+ * are applied to the side model that has not been mutated. Ultimately, this test checks whether both
+ * resulting models are equal to each other, which should be true since both versions should correspond to the
+ * mutated model version.
  * </p>
  * 
  * @author Philip Langer <planger@eclipsesource.com>
@@ -89,11 +84,21 @@ public class ThreeWayFuzzyTest {
 
 	private EObject rightRootObject;
 
+	private boolean mutateLeft;
+
 	@Before
 	public void prepareTwoVersions() {
 		createOriginRootObject();
-		createLeftRootObject();
-		createRightRootObject();
+		mutateLeft = new Random(mutateUtil.getSeed()).nextBoolean();
+		if (mutateLeft) {
+			createRightRootObject();
+			mutateUtil.mutate();
+			createLeftRootObject();
+		} else {
+			createLeftRootObject();
+			mutateUtil.mutate();
+			createRightRootObject();
+		}
 	}
 
 	private void createOriginRootObject() {
@@ -102,13 +107,11 @@ public class ThreeWayFuzzyTest {
 	}
 
 	private void createLeftRootObject() {
-		mutateUtil.mutate();
 		removeAllDuplicateCrossReferences(generatedRootObject);
 		saveLeftRootObject(generatedRootObject);
 	}
 
 	private void createRightRootObject() {
-		mutateUtil.mutate();
 		removeAllDuplicateCrossReferences(generatedRootObject);
 		saveRightRootObject(generatedRootObject);
 	}
@@ -143,6 +146,10 @@ public class ThreeWayFuzzyTest {
 		final Notifier left = getLeftNotifier();
 		final Notifier right = getRightNotifier();
 
+//		mutateUtil.saveEObject((EObject)origin, "origin_", false);
+//		mutateUtil.saveEObject((EObject)left, "left_", false);
+//		mutateUtil.saveEObject((EObject)right, "right_", false);
+
 		// perform comparison
 		final IComparisonScope scope = new DefaultComparisonScope(left, right, origin);
 		Comparison comparison = EMFCompare.builder().build().compare(scope);
@@ -151,15 +158,13 @@ public class ThreeWayFuzzyTest {
 		// shuffle differences to cover different orders of diffs to be merged
 		Collections.shuffle(differences, new Random(mutateUtil.getSeed()));
 
+		// merge mutated side into non-mutated side
 		final IBatchMerger merger = new BatchMerger(mergerRegistry);
-
-		// apply all non-conflicting changes to left (right-to-left)
-		final List<Diff> rightNonConflictingDiffs = filterDiffs(differences, DifferenceSource.RIGHT, true);
-		merger.copyAllRightToLeft(rightNonConflictingDiffs, new BasicMonitor());
-
-		// apply all changes to right (left-to-right)
-		final List<Diff> leftDiffs = filterDiffs(differences, DifferenceSource.LEFT, false);
-		merger.copyAllLeftToRight(leftDiffs, new BasicMonitor());
+		if (mutateLeft) {
+			merger.copyAllLeftToRight(differences, new BasicMonitor());
+		} else {
+			merger.copyAllRightToLeft(differences, new BasicMonitor());
+		}
 
 		// check for differences between left and right
 		final IComparisonScope assertionScope = new DefaultComparisonScope(left, right, null);
@@ -167,17 +172,6 @@ public class ThreeWayFuzzyTest {
 		final EList<Diff> assertionDiffs = assertionComparison.getDifferences();
 
 		assertTrue(assertionDiffs.isEmpty());
-	}
-
-	private List<Diff> filterDiffs(List<Diff> diffs, DifferenceSource source, boolean filterConflicting) {
-		List<Diff> filteredDiffs = new ArrayList<Diff>();
-		for (Diff diff : diffs) {
-			boolean isConflicting = diff.getConflict() != null && ConflictKind.REAL.equals(diff.getConflict().getKind());
-			if (source.equals(diff.getSource()) && (!isConflicting || !filterConflicting)) {
-				filteredDiffs.add(diff);
-			}
-		}
-		return filteredDiffs;
 	}
 
 	protected void removeAllDuplicateCrossReferences(EObject contentRoot) {
